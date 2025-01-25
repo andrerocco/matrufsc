7b1316a14b28d5701940d2ea7d317d9ba23671d9
Andre Rocco <99764757+andrerocco@users.noreply.github.com>
2025-01-24 21:37:13 -0300

    fix: add path aliases in TypeScript and Vite configuration

--- matrufsc/vite.config.ts
+++ matrufsc/vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+// Plugins
 import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), visualizer({ open: true })],
+    resolve: {
+        /* Aliases (also change tsconfig.app.json) */
+        alias: {
+            "~": path.resolve(__dirname, "src"),
+        },
+    },
 });
